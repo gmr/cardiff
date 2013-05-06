edf54d64fff14671fb4e1bfda351006c2b3bf466
Gavin M. Roy <gmr@meetme.com>
2013-05-06 10:07:36 -0400

    Prepping for pypi upload

--- setup.py
+++ setup.py
@@ -8,6 +8,17 @@ requirements = ['clihelper',
                 'pyyaml',
                 'tornado']
 
+# Build the path to install the templates, example config and static files
+base_path = '/usr/local/share/cardiff'
+
+data_files = {'%s/' % base_path: ['README.md', 'etc/example.yaml'],
+              '%s/init.d/' % base_path: ['etc/init.d/cardiff']}
+
+with open('MANIFEST.in', 'w') as handle:
+    for path in data_files:
+        for filename in data_files[path]:
+            handle.write('include %s\n' % filename)
+
 tests_require = ['mock']
 (major, minor, rev) = platform.python_version_tuple()
 if float('%s.%s' % (major, minor)) < 2.7:
@@ -15,10 +26,10 @@ if float('%s.%s' % (major, minor)) < 2.7:
 
 setup(name='cardiff',
       version='1.0.0',
-      description='RabbitMQ Consumer Framework',
+      description='Python statsd clone',
       author='Gavin M. Roy',
       author_email='gavinmroy@gmail.com',
-      url='http://cardiff.readthedocs.org',
+      url='http://github.com/gmr/cardiff',
       packages=['cardiff', 'cardiff.backends'],
       license='BSD',
       classifiers=[
@@ -39,5 +50,6 @@ setup(name='cardiff',
       install_requires=requirements,
       extras_require={'amqp': ['rmqid']},
       tests_require=tests_require,
+      data_files=[(key, data_files[key]) for key in data_files.keys()],
       entry_points=dict(console_scripts=scripts),
       zip_safe=True)
